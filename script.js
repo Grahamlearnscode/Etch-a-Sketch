5c368ec400bf7a22f0fcf602933ec167730549c7
GrahamLearnsCode <grahamlearnscode@gmail.com>
2020-03-31 21:44:19 +0100

    add function to create grid with x rows and y columns

--- script.js
+++ script.js
@@ -3,17 +3,22 @@
 const wrapper=document.querySelector('#wrapper');
 const gridStyle="display: inline-block; margin-left: 1px; height: 100px; width: 100px; background-color: blue";
 
-createGrid(16, 16);
+createGrid(4, 6);
 
 function createGrid(xGrid, yGrid) {
     console.log('creating ' + xGrid + ' x ' + yGrid + ' grid');
+    ///repeat y times
+    for (y=0; y<yGrid; y++) {
+        createXGrid(xGrid);
+        const br=document.createElement('br');
+        wrapper.appendChild(br);
+        }
+    }
 
 
-    let singleGrid=document.createElement('div');
-    singleGrid.setAttribute('style', gridStyle); //blue squares
-    console.log('adding grid');
-    wrapper.appendChild(singleGrid);
-    let secondGrid=document.createElement('div');
-    secondGrid.setAttribute('style', gridStyle);
-    wrapper.appendChild(secondGrid);
-}
+
+function createXGrid(xGrid) {
+    for (x=0; x<xGrid; x++) {
+        let singleGrid=document.createElement('div');
+        singleGrid.setAttribute('style', gridStyle); //blue squares
+        wrapper.appendChild(singleGrid);}}
